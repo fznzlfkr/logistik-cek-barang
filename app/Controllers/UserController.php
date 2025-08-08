4ce573340583a28ad77e96faf770abbef172c3f3
miracleverytime <faizrizqullah2@gmail.com>
2025-08-08 10:26:40 +0700

    feat(user): tampilan dashboard sudah real-time dari database

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -3,17 +3,50 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
+use App\Models\BarangModel;
+use App\Models\LaporanModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class UserController extends BaseController
 {
+    protected $userModel,
+        $barangModel,
+        $laporanModel;
+
+    public function __construct()
+    {
+        $this->userModel = new UserModel;
+        $this->barangModel = new BarangModel;
+        $this->laporanModel = new LaporanModel;
+    }
+
     public function index()
     {
+        $totalMasuk = $this->laporanModel->where('jenis', 'Masuk')->countAllResults();
+        $totalDipakai = $this->laporanModel->where('jenis', 'Dipakai')->countAllResults();
+        $totalBarang = $this->barangModel->selectSum('jumlah')->get()->getRow()->jumlah;
+        $barangMinimum = $this->barangModel->where('jumlah <= minimum_stok')->countAllResults();
+
+        $laporanData = $this->laporanModel
+            ->select('laporan.tanggal, barang.nama_barang, laporan.jenis, laporan.jumlah')
+            ->join('barang', 'barang.id_barang = laporan.id_barang')
+            ->orderBy('laporan.tanggal', 'DESC')
+            ->limit(10)
+            ->findAll();
+
         $data = [
             'title' => 'Dashboard User - CargoWing',
+            'totalMasuk' => $totalMasuk,
+            'totalDipakai' => $totalDipakai,
+            'totalBarang' => $totalBarang,
+            'barangMinimum' => $barangMinimum,
+            'laporanData' => $laporanData,
         ];
+
         return view('user/dashboard', $data);
     }
+
     public function kelolaBarang()
     {
         $data = [
@@ -21,6 +54,7 @@ class UserController extends BaseController
         ];
         return view('user/kelola_barang', $data);
     }
+
     public function riwayat()
     {
         $data = [
@@ -28,6 +62,7 @@ class UserController extends BaseController
         ];
         return view('user/riwayat', $data);
     }
+
     public function profil()
     {
         $data = [
